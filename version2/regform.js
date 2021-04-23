b97b93c2a92f4caf1cdcaf8c6deb3d08cfa4de30
sobhakhadka98 <sobhakhadka98@gmail.com>
2021-04-24 03:02:15 +0530

    making form interactive

---
+++ version2/regform.js
@@ -0,0 +1,40 @@
+var repeat = document.getElementById("repeat");
+var courses = document.querySelector("#courses");
+var spec = document.querySelector("#spec");
+var spec2 = document.querySelector("#spec2");
+var bachelor = document.querySelector("#bachelor");
+
+repeat.onclick = () => {
+    if(repeat.checked == true){
+        document.getElementById("street2").value = document.getElementById("street1").value;
+        document.getElementById("city2").value = document.getElementById("city1").value;
+        document.getElementById("state2").value = document.getElementById("state1").value;
+        document.getElementById("zip2").value = document.getElementById("zip1").value;
+        document.getElementById("country2").value = document.getElementById("country1").value;
+    } else {
+        document.getElementById("street2").value ="";
+        document.getElementById("city2").value ="";
+        document.getElementById("state2").value = "";
+        document.getElementById("zip2").value = null;
+        document.getElementById("country2").value = "";
+    }
+}
+
+courses.onclick = () => {
+    if (courses.selectedIndex == 1 || courses.selectedIndex == 4)
+    {
+        spec.classList.remove("specialization");
+        spec2.classList.remove("specialization");
+    }
+    else 
+    {
+        spec.classList.add("specialization");
+        spec2.classList.add("specialization");
+    }
+    if(courses.selectedIndex == 4 || courses.selectedIndex == 5){
+        bachelor.classList.remove("Bachelors");
+    } else {
+        bachelor.classList.add("Bachelors");
+    }
+};
+
